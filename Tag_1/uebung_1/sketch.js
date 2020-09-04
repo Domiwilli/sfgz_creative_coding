913efb8ff2cf5a3c175210622c54341ca0c1a233
Dominique Willi <willidominique@gmail.com>
2020-09-04 13:38:01 +0200

    docs

---
+++ Tag_1/uebung_1/sketch.js
@@ -0,0 +1,13 @@
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220, 100, 100);
+  fill(255, 204, 100);
+  ellipse(200,200,100,100);
+  fill(200, 204, 100);
+  rect(195,100,10,200);
+  noStroke();
+}
