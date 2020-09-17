e3e0eb73c05a886e990c12eff880b2c0986ec488
Dominique Willi <willidominique@gmail.com>
2020-09-17 23:02:49 +0200

    add ellipses

--- Tag_1/uebung_1/sketch.js
+++ Tag_1/uebung_1/sketch.js
@@ -4,10 +4,161 @@ function setup() {
 }
 
 function draw() {
-  background(220, 100, 100);
-  fill(255, 204, 100);
-  ellipse(200,200,100,100);
-  fill(200, 204, 100);
-  rect(195,100,10,200);
+  background(255, 113, 91);
+
+  fill(252, 252, 252, 50);
+
+  ellipse(50, 50, 100, 100)
   noStroke();
+
+  ellipse(350, 350, 100, 100)
+  noStroke();
+
+  ellipse(350, 50, 100, 100)
+  noStroke();
+
+  ellipse(50, 350, 100, 100)
+  noStroke();
+
+
+  ellipse(60, 60, 100, 100)
+  noStroke();
+
+  ellipse(340, 340, 100, 100)
+  noStroke();
+
+  ellipse(340, 60, 100, 100)
+  noStroke();
+
+  ellipse(60, 340, 100, 100)
+  noStroke();
+
+
+  ellipse(70, 70, 100, 100)
+  noStroke();
+
+  ellipse(330, 330, 100, 100)
+  noStroke();
+
+  ellipse(330, 70, 100, 100)
+  noStroke();
+
+  ellipse(70, 330, 100, 100)
+  noStroke();
+
+
+  ellipse(80, 80, 100, 100)
+  noStroke();
+
+  ellipse(320, 320, 100, 100)
+  noStroke();
+
+  ellipse(320, 80, 100, 100)
+  noStroke();
+
+  ellipse(80, 320, 100, 100)
+  noStroke();
+
+
+  ellipse(90, 90, 100, 100)
+  noStroke();
+
+  ellipse(310, 310, 100, 100)
+  noStroke();
+
+  ellipse(310, 90, 100, 100)
+  noStroke();
+
+  ellipse(90, 310, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 50, 100, 100)
+  noStroke();
+
+  ellipse(50, 200, 100, 100)
+  noStroke();
+
+  ellipse(200, 350, 100, 100)
+  noStroke();
+
+  ellipse(350, 200, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 60, 100, 100)
+  noStroke();
+
+  ellipse(60, 200, 100, 100)
+  noStroke();
+
+  ellipse(200, 340, 100, 100)
+  noStroke();
+
+  ellipse(340, 200, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 70, 100, 100)
+  noStroke();
+
+  ellipse(70, 200, 100, 100)
+  noStroke();
+
+  ellipse(200, 330, 100, 100)
+  noStroke();
+
+  ellipse(330, 200, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 80, 100, 100)
+  noStroke();
+
+  ellipse(80, 200, 100, 100)
+  noStroke();
+
+  ellipse(200, 320, 100, 100)
+  noStroke();
+
+  ellipse(320, 200, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 90, 100, 100)
+  noStroke();
+
+  ellipse(90, 200, 100, 100)
+  noStroke();
+
+  ellipse(200, 310, 100, 100)
+  noStroke();
+
+  ellipse(310, 200, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 100, 100, 100)
+  noStroke();
+
+  ellipse(100, 200, 100, 100)
+  noStroke();
+
+  ellipse(200, 300, 100, 100)
+  noStroke();
+
+  ellipse(300, 200, 100, 100)
+  noStroke();
+
+
+  ellipse(200, 200, 12, 12)
+  noStroke();
+
+  ellipse(200, 200, 30, 30)
+  noStroke();
+
+  ellipse(200, 200, 50, 50)
+  noStroke();
+
+
 }
