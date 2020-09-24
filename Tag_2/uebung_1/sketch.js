88397b575744cf842f489a44d4d823bd75a804c6
Dominique Willi <willidominique@gmail.com>
2020-09-24 11:42:02 +0200

    loops

--- Tag_2/uebung_1/sketch.js
+++ Tag_2/uebung_1/sketch.js
@@ -5,11 +5,31 @@ function setup() {
 
 function draw() {
 
-  strokeWeight(12);
-  background(220);
+  strokeWeight(2);
+  background(194, 223, 227);
 
-  for(let x=1;x<=11;x++){
-      rect(25*x,25*x,500,500);
+  for(let x=1;x<=5;x++){
+    stroke(255);
+      rect(25*x,25*x,200,400);
+      }
+
+  for(let x=1;x<=5;x++){
+    stroke(255);
+      rect(25*x,25*x,400,200);
 
       }
+
+  for(let x=2;x<=10;x++){
+    stroke(255);
+      rect(25*x,25*x,400,200);
+
+      }
+
+  for(let x=2;x<=10;x++){
+    stroke(255);
+      rect(25*x,25*x,200,400);
+
+      }
+
+
 }
