97ae19bd3ed0c2b48da9b7c0759e12dc722b68fa
Dominique Willi <willidominique@gmail.com>
2020-10-03 13:13:38 +0200

    add neue form

--- Tag_3/uebung_4/sketch.js
+++ Tag_3/uebung_4/sketch.js
@@ -10,6 +10,18 @@ function draw() {
   stroke(215, 190, 168);
 
   beginShape();
+  vertex(0, 0);
+  quadraticVertex(0, 50, 50, 50);
+  quadraticVertex(100, 50, 100, 100);
+  endShape(CLOSE)
+
+  beginShape();
+  vertex(0, 100);
+  quadraticVertex(50, 100, 50, 50);
+  quadraticVertex(50, 0, 100, 0);
+  endShape(CLOSE)
+
+  beginShape();
   vertex(100, 100);
   quadraticVertex(100, 50, 150, 50);
   quadraticVertex(200, 50, 200, 0);
