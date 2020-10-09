b50a60bbe4f87bfa29b7a3605b242556f44ef353
Dominique Willi <willidominique@gmail.com>
2020-10-09 16:10:17 +0200

    include more wind

--- Tag_3/uebung_4/sketch.js
+++ Tag_3/uebung_4/sketch.js
@@ -1,44 +1,54 @@
 var gui;
 
-var distanz = 150;
-
+var distanz = 50;
+var rotation = 360;
+var groesse = 300;
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
   angleMode(DEGREES);
 
-  sliderRange(100, 150);
+  sliderRange(100, 200);
   var gui = createGui('p5.gui');
-  gui.addGlobals('distanz');
+  gui.addGlobals('distanz','rotation','groesse');
   noLoop();
 }
 
 function draw() {
-  background(255, 255, 255, 10);
+  background(0);
 
   noFill();
   stroke(215, 190, 168);
 
+
   for (let x = 0; x < width; x += distanz) {
     for (let y = 0; y < height; y += distanz) {
 
-      push();
+
+  push();
+  translate(50 + x, 50 + y);
+  rotate(rotation + x + y);
+  translate(-50 + x, -50 + y);
+
       beginShape();
-      translate(50, 50);
-      rotate();
       vertex(0 + x, 0 + y);
       quadraticVertex(0 + x, 50 + y, 50 + x, 50 + y);
       quadraticVertex(100 + x, 50 + y, 100 + x, 100 + y);
       endShape(CLOSE)
-      pop();
+  pop();
 
+
+    push();
       beginShape();
+      translate(50 + x, 50 + y);
+      rotate(rotation + x + y);
+      translate(-50 + x, -50 + y);
       vertex(0 + x, 100 + y);
       quadraticVertex(50 + x, 100 + y, 50 + x, 50 + y);
       quadraticVertex(50 + x, 0 + y, 100 + x, 0 + y);
       endShape(CLOSE)
 
-
+  pop();
 
       /*beginShape();
       vertex(0, 100);
